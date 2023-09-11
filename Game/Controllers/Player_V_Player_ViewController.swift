267a87f57fceb54bef3c94b4c4fcd0c542a517ca
DavidSpartan95 <112651588+DavidSpartan95@users.noreply.github.com>
2023-09-11 17:43:03 +0200

    Added Navigation & Screens
    Mint colored theme, added navigation and screen to new screen were user can name both player

---
+++ Game/Controllers/Player_V_Player_ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Player_V_Player_ViewController.swift
+//  Game
+//
+//  Created by David Ulvan on 2023-09-11.
+//
+
+import UIKit
+
+class Player_V_Player_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Game/Controllers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by David Ulvan on 2023-09-11.
+//
+
+import UIKit
+
+let NAV_TO_PVP = "navToPvP"
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == NAV_TO_PVP {
+            let destVC = segue.destination as! Player_V_Player_ViewController
+        }
+    }
+
+    @IBAction func onPvPclick(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onPvEclick(_ sender: Any) {
+        
+    }
+    
+}
+

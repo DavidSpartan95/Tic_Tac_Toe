2fe4b477f6bdf47babdfa1fa7a99fc826b9d687e
DavidSpartan95 <112651588+DavidSpartan95@users.noreply.github.com>
2023-09-13 10:31:10 +0200

    Game Board progress
    Can now place X and O on the board based on who's turn it is. Also simplified/shorten the code by removing redundant code

--- Game/Controllers/GameViewController.swift
+++ Game/Controllers/GameViewController.swift
@@ -13,28 +13,18 @@ import UIKit
 
 class GameViewController: UIViewController {
     
+    @IBOutlet var boardImageViews: [UIImageView]!
     @IBOutlet weak var lblPlayerX: UILabel!
     @IBOutlet weak var lblPlayerO: UILabel!
     
-    @IBOutlet weak var A1: UIImageView!
-    @IBOutlet weak var A2: UIImageView!
-    @IBOutlet weak var A3: UIImageView!
-    @IBOutlet weak var B1: UIImageView!
-    @IBOutlet weak var B2: UIImageView!
-    @IBOutlet weak var B3: UIImageView!
-    @IBOutlet weak var C1: UIImageView!
-    @IBOutlet weak var C2: UIImageView!
-    @IBOutlet weak var C3: UIImageView!
-    
-    var boardImageViews: [UIImageView] = []
-    
+    //Board that remembers all the markers has been placed
     var board = Array(repeating: marker.empty, count: 9)
     var playerXname: String?
     var playerOname: String?
+    var playerXturn = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        boardImageViews = [A1, A2, A3, B1, B2, B3, C1, C2, C3]
         
         if let playerXname = playerXname {
             lblPlayerX.text = playerXname
@@ -42,46 +32,24 @@ class GameViewController: UIViewController {
         if let playerOname = playerOname {
             lblPlayerO.text = playerOname
         }
-        // Do any additional setup after loading the view.
-    }
-   
-    @IBAction func tapA1(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapA2(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapA3(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapB1(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapB2(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapB3(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapC1(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapC2(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
-    }
-    @IBAction func tapC3(_ sender: Any) {
-        placeMarker(tag: getTag(sender), symbol: marker.X)
+    
     }
     
-    func placeMarker(tag:Int, symbol: marker){
-        let image = (symbol == marker.X) ? "xmark.circle.fill" : "circle.fill"
+    @IBAction func tapOnBoard(_ sender: UITapGestureRecognizer) {
+        print(getTag(sender))
+        placeMarker(tag: getTag(sender), playerXturn: playerXturn)
+        playerXturn.toggle()
+    }
+    
+    func placeMarker(tag:Int, playerXturn: Bool){
+        let symbol = (playerXturn.self) ? marker.X : marker.O
+        let image = (symbol == marker.X) ? "xmark" : "circle"
         guard tag >= 0 && tag < boardImageViews.count else { return }
             boardImageViews[tag].image = UIImage(systemName: image)
     }
     
-    func getTag(_ sender: Any) -> Int{
-        if let tapGesture = sender as? UITapGestureRecognizer,
-           let imageView = tapGesture.view as? UIImageView {
+    func getTag(_ tapGesture: UITapGestureRecognizer) -> Int{
+        if let imageView = tapGesture.view as? UIImageView {
             let tag = imageView.tag
             return tag
         }
